582f38ce1e998a91fc0bc61e37c1e6d874c8f567
Lucas MVO <lucas.matos.v.o@gmail.com>
2025-04-18 13:19:16 -0300

    Trying to fix project deploy

--- AceSchedules_frontend/vite.config.ts
+++ AceSchedules_frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
         target: 'http://localhost:5001/',
         changeOrigin: true,
         secure: false,
+        protocolRewrite: 'http',
       },
     },
 		host: 'localhost'
