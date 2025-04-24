7d04e232a0d777ab4d279754936755d5e42bc061
Lucas MVO <lucas.matos.v.o@gmail.com>
2025-04-24 18:17:44 -0300

    Adjusting deployment

--- AceSchedules_frontend/vite.config.ts
+++ AceSchedules_frontend/vite.config.ts
@@ -14,6 +14,10 @@ export default defineConfig({
     },
 		host: 'localhost'
   },
+  build: {
+    outDir: '../dist/client',
+    emptyOutDir: true
+  },
   plugins: [react()],
   base: '/AceSchedules/'
 })
